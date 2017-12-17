d4702b1c326980d71b6ab1aa4fdee1d50aa44b0e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-12-17 18:39:37 -0200

    cedric: Use Pixel build fingerprint to bypass safetynet

--- lineage.mk
+++ lineage.mk
@@ -31,6 +31,6 @@ PRODUCT_NAME := lineage_cedric
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=motorola/cedric/cedric:7.0/NPPS25.137-72-4/4:user/release-keys \
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
     PRIVATE_BUILD_DESC="cedric-7.0/NPPS25.137-72-4/4:user/release-keys" \
     PRODUCT_NAME="Moto G5"
